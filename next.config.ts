b44cafda9660b2bea511e55817595646c42b8466
macbook <macbook@macbook-MacBook-Pro.local>
2026-08-13 09:09:00 +0300

    Include logo in Vercel server bundle

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    '/*': ['./public/logo/universite-labe.png'],
+  },
+};
 
 export default nextConfig;
